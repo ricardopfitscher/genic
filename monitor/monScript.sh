c2fa850c8019bbe65c665defcfee95f0ea5b8aef
Ricardo Pfitscher <ricardo.pfitscher@gmail.com>
2018-02-14 15:40:26 -0200

    update monitor

--- monitor/monScript.sh
+++ monitor/monScript.sh
@@ -5,22 +5,23 @@
 #$2 is the vnt type
 #$3 is the iteration
 #$4 is time for monitoring
+#$5 is the call rate
 
 #apt-get install sysstat
 #apt-get install perf-tools-unstable
-sar -A 1 $4 >> output-sarAll-$1-$2-$3.dat &
-./cachestat >> output-cache-$1-$2-$3.dat &
-mpstat 1 $4 >> output-mpstat-$1-$2-$3.dat &
-iostat 1 $4 >> output-iostat-$1-$2-$3.dat &
-ifstat 1 $4 >> output-ifstat-$1-$2-$3.dat &
-sar -r 1 $4 >> output-sarMem-$1-$2-$3.dat &
+sar -A 1 $4 >> output-sarAll-$1-$2-$3-$5.dat &
+./cachestat >> output-cache-$1-$2-$3-$5.dat &
+mpstat 1 $4 >> output-mpstat-$1-$2-$3-$5.dat &
+iostat 1 $4 >> output-iostat-$1-$2-$3-$5.dat &
+ifstat 1 $4 >> output-ifstat-$1-$2-$3-$5.dat &
+sar -r 1 $4 >> output-sarMem-$1-$2-$3-$5.dat &
 
 typeset -i i
 for ((i=1;i<=$4;++i))
 do
-	tc -s -d qdisc show dev eth0 |grep backlog >> output-tc-$1-$2-$3.dat
-	ethtool -S eth0 | grep dropp >> output-ethDropp-$1-$2-$3.dat
-	ifconfig eth0 | grep dropp >> output-ethDropp2-$1-$2-$3.dat
+	tc -s -d qdisc show dev eth0 |grep backlog >> output-tc-$1-$2-$3-$5.dat
+	ethtool -S eth0 | grep dropp >> output-ethDropp-$1-$2-$3-$5.dat
+	ifconfig eth0 | grep dropp >> output-ethDropp2-$1-$2-$3-$5.dat
 
 	echo $i
 	sleep 1
